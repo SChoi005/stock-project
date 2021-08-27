506322afb59a6f027837d7236af2445945e1ad30
SChoi005 <pill21361@gmail.com>
2021-08-27 04:29:40 +0000

    nothing

--- stock-project-front/src/setupProxy.js
+++ stock-project-front/src/setupProxy.js
@@ -1,10 +1,11 @@
-const {createProxyMiddleware} = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://0.0.0.0:8080',
+            secure: false,
+            target: 'http://localhost:8080',
             changeOrigin: true
         })
     );
